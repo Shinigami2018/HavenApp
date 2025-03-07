a3dfe061807e9eb42b678dd9c14bbf650b67afe2
Shinigami2018 <sameenpcc2018@gmail.com>
2025-03-07 16:52:29 +0600

    Implemented SMS(finally)
    SMS code working, further refining left

--- src/main/java/com/haven/app/haven/send_sms.py
+++ src/main/java/com/haven/app/haven/send_sms.py
@@ -1,5 +1,5 @@
 import yagmail
 
-yag = yagmail.SMTP("sameenpcc2018@gmail.com", "fhae rkkc srki vgpt")
+yag = yagmail.SMTP("havenappreplies@gmail.com", "qlpm malu nxjp gzrc")
 yag.send("arifulm926@gmail.com", "Subject Here", "I failed, I learned - Khaled Sir")
 print("Email sent!")
