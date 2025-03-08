9604fba6f4777dbfd0ec7e0ff17c488f5d30adb3
AsifRahman114 <asifrifat143@gmail.com>
2025-03-08 08:08:44 +0600

    added dynamic resizing for journal page

--- src/main/java/com/haven/app/haven/JournalFxmlController.java
+++ src/main/java/com/haven/app/haven/JournalFxmlController.java
@@ -1,10 +1,17 @@
 package com.haven.app.haven;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import java.io.*;
+
+import static com.haven.app.haven.HelloApplication.primaryStage;
 
 public class JournalFxmlController {
 
@@ -12,6 +19,11 @@ public class JournalFxmlController {
     public TextArea journalTextArea;
 
     public final String defaultFilePath = "journal.txt";  // Default file
+    public VBox sidebar;
+    public VBox Editor;
+    public StackPane bottom;
+    public JFXButton saveButton;
+    public JFXButton loadButton;
 
     /**
      * Saves the journal text to a file chosen by the user.
@@ -56,5 +68,29 @@ public class JournalFxmlController {
                 e.printStackTrace();
             }
         }
+    }
+
+
+    @FXML
+    public void initialize() {
+        // Make sidebar width adjust dynamically (take 20% of window width)
+        sidebar.prefWidthProperty().bind(primaryStage.widthProperty().multiply(0.17));
+        // Make the editor dynamically resize
+        Editor.prefWidthProperty().bind(primaryStage.widthProperty().multiply(0.75)); // 75% of window width
+
+        // Make the text area take full width inside Editor
+        journalTextArea.prefWidthProperty().bind(Editor.widthProperty().subtract(20));//
+        bottom.prefWidthProperty().bind(primaryStage.widthProperty().multiply(1));
+        saveButton.prefWidthProperty().bind(primaryStage.widthProperty().multiply(0.08));
+        loadButton.prefWidthProperty().bind(primaryStage.widthProperty().multiply(0.08));// Add margin
+
+
+    }
+
+
+    public void refreshFileList(ActionEvent event) {
+    }
+
+    public void loadSelectedFile(MouseEvent mouseEvent) {
     }
 }
